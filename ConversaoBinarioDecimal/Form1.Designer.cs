a262d76f3f71e3dc4ca54eb7e9f0da5b0542e15a
edjanilson florencio <edjanilsonflorencioads@gmail.com>
2021-01-07 20:51:56 -0300

    Iniciando um projeto no github

---
+++ ConversaoBinarioDecimal/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace ConversaoBinarioDecimal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.txtNum2 = new System.Windows.Forms.TextBox();
+            this.txtNum3 = new System.Windows.Forms.TextBox();
+            this.txtNum4 = new System.Windows.Forms.TextBox();
+            this.txtNum5 = new System.Windows.Forms.TextBox();
+            this.txtNum6 = new System.Windows.Forms.TextBox();
+            this.txtNum7 = new System.Windows.Forms.TextBox();
+            this.txtResultadoFinal = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.btnApagar = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNum0 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtNum1
+            // 
+            this.txtNum1.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNum1.Location = new System.Drawing.Point(517, 95);
+            this.txtNum1.MaxLength = 1;
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(78, 27);
+            this.txtNum1.TabIndex = 1;
+            this.txtNum1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtNum2
+            // 
+            this.txtNum2.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNum2.Location = new System.Drawing.Point(433, 95);
+            this.txtNum2.MaxLength = 1;
+            this.txtNum2.Name = "txtNum2";
+            this.txtNum2.Size = new System.Drawing.Size(78, 27);
+            this.txtNum2.TabIndex = 2;
+            this.txtNum2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtNum3
+            // 
+            this.txtNum3.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNum3.Location = new System.Drawing.Point(349, 95);
+            this.txtNum3.MaxLength = 1;
+            this.txtNum3.Name = "txtNum3";
+            this.txtNum3.Size = new System.Drawing.Size(78, 27);
+            this.txtNum3.TabIndex = 3;
+            this.txtNum3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtNum4
+            // 
+            this.txtNum4.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNum4.Location = new System.Drawing.Point(265, 95);
+            this.txtNum4.MaxLength = 1;
+            this.txtNum4.Name = "txtNum4";
+            this.txtNum4.Size = new System.Drawing.Size(78, 27);
+            this.txtNum4.TabIndex = 4;
+            this.txtNum4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtNum5
+            // 
+            this.txtNum5.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNum5.Location = new System.Drawing.Point(181, 95);
+            this.txtNum5.MaxLength = 1;
+            this.txtNum5.Name = "txtNum5";
+            this.txtNum5.Size = new System.Drawing.Size(78, 27);
+            this.txtNum5.TabIndex = 5;
+            this.txtNum5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtNum6
+            // 
+            this.txtNum6.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNum6.Location = new System.Drawing.Point(97, 95);
+            this.txtNum6.MaxLength = 1;
+            this.txtNum6.Name = "txtNum6";
+            this.txtNum6.Size = new System.Drawing.Size(78, 27);
+            this.txtNum6.TabIndex = 6;
+            this.txtNum6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtNum7
+            // 
+            this.txtNum7.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNum7.Location = new System.Drawing.Point(12, 95);
+            this.txtNum7.MaxLength = 1;
+            this.txtNum7.Name = "txtNum7";
+            this.txtNum7.Size = new System.Drawing.Size(78, 27);
+            this.txtNum7.TabIndex = 7;
+            this.txtNum7.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtResultadoFinal
+            // 
+            this.txtResultadoFinal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResultadoFinal.Location = new System.Drawing.Point(265, 210);
+            this.txtResultadoFinal.Name = "txtResultadoFinal";
+            this.txtResultadoFinal.ReadOnly = true;
+            this.txtResultadoFinal.Size = new System.Drawing.Size(127, 26);
+            this.txtResultadoFinal.TabIndex = 11;
+            this.txtResultadoFinal.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(229, 187);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(198, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Resultado em Decimal:\r\n";
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFechar.Image = global::ConversaoBinarioDecimal.Properties.Resources.iconfinder_17_330399;
+            this.btnFechar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnFechar.Location = new System.Drawing.Point(389, 305);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(98, 37);
+            this.btnFechar.TabIndex = 10;
+            this.btnFechar.Text = "Fechar";
+            this.btnFechar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnFechar.UseVisualStyleBackColor = true;
+            this.btnFechar.Click += new System.EventHandler(this.btnFechar_Click);
+            // 
+            // btnApagar
+            // 
+            this.btnApagar.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnApagar.Image = global::ConversaoBinarioDecimal.Properties.Resources.iconfinder_edit_clear_23227__1_;
+            this.btnApagar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnApagar.Location = new System.Drawing.Point(287, 305);
+            this.btnApagar.Name = "btnApagar";
+            this.btnApagar.Size = new System.Drawing.Size(96, 37);
+            this.btnApagar.TabIndex = 9;
+            this.btnApagar.Text = "Apagar";
+            this.btnApagar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnApagar.UseVisualStyleBackColor = true;
+            this.btnApagar.Click += new System.EventHandler(this.btnApagar_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Image = global::ConversaoBinarioDecimal.Properties.Resources.iconfinder_Cancel_Icon_919983__1_;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(175, 305);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 37);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Calcular";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(142, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(378, 28);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Insira um número em binário:";
+            // 
+            // txtNum0
+            // 
+            this.txtNum0.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNum0.Location = new System.Drawing.Point(601, 95);
+            this.txtNum0.MaxLength = 1;
+            this.txtNum0.Name = "txtNum0";
+            this.txtNum0.Size = new System.Drawing.Size(78, 27);
+            this.txtNum0.TabIndex = 0;
+            this.txtNum0.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(695, 354);
+            this.Controls.Add(this.btnFechar);
+            this.Controls.Add(this.btnApagar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtResultadoFinal);
+            this.Controls.Add(this.txtNum7);
+            this.Controls.Add(this.txtNum6);
+            this.Controls.Add(this.txtNum5);
+            this.Controls.Add(this.txtNum4);
+            this.Controls.Add(this.txtNum3);
+            this.Controls.Add(this.txtNum2);
+            this.Controls.Add(this.txtNum0);
+            this.Controls.Add(this.txtNum1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Conversão - Binário - Decimal";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtNum1;
+        private System.Windows.Forms.TextBox txtNum2;
+        private System.Windows.Forms.TextBox txtNum3;
+        private System.Windows.Forms.TextBox txtNum4;
+        private System.Windows.Forms.TextBox txtNum5;
+        private System.Windows.Forms.TextBox txtNum6;
+        private System.Windows.Forms.TextBox txtNum7;
+        private System.Windows.Forms.TextBox txtResultadoFinal;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnApagar;
+        private System.Windows.Forms.Button btnFechar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNum0;
+    }
+}
+
---
+++ ConversaoBinarioDecimal/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConversaoBinarioDecimal
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (txtNum0.Text == "" || txtNum1.Text == "" || txtNum2.Text == "" || txtNum3.Text == "" || txtNum4.Text == "" || txtNum5.Text == "" || txtNum6.Text == "" || txtNum7.Text == "")
+            {
+                MessageBox.Show("Dados ausentes. Insira todos os números e tente novamente.", "====Atenção====", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                try
+                {
+
+                    double valor_total;
+
+                    //Recebe um número em binário
+                    int num0, num1, num2, num3, num4, num5, num6, num7;
+                    num0 = int.Parse(txtNum0.Text);
+                    num1 = int.Parse(txtNum1.Text);
+                    num2 = int.Parse(txtNum2.Text);
+                    num3 = int.Parse(txtNum3.Text);
+                    num4 = int.Parse(txtNum4.Text);
+                    num5 = int.Parse(txtNum5.Text);
+                    num6 = int.Parse(txtNum6.Text);
+                    num7 = int.Parse(txtNum7.Text);
+
+                    //Realiza a potenciação
+                    double doiselevadoazero = Math.Pow(2, 0);
+                    double doiselevadoaum = Math.Pow(2, 1);
+                    double doiselevadoadois = Math.Pow(2, 2);
+                    double doiselevadoatres = Math.Pow(2, 3);
+                    double doiselevadoaquatro = Math.Pow(2, 4);
+                    double doiselevadoacinco = Math.Pow(2, 5);
+                    double doiselevadoaseis = Math.Pow(2, 6);
+                    double doiselevadoasete = Math.Pow(2, 7);
+
+                    if (num0<2 && num1<2 && num2<2 && num3 < 2 && num4 < 2 && num5 < 2 && num6 < 2 && num7 < 2)
+                    {
+
+
+                        //Junta o cálculo da potenciação com o número binário em questão
+                        double valor1 = doiselevadoazero * num0;
+                        double valor2 = doiselevadoaum * num1;
+                        double valor3 = doiselevadoadois * num2;
+                        double valor4 = doiselevadoatres * num3;
+                        double valor5 = doiselevadoaquatro * num4;
+                        double valor6 = doiselevadoacinco * num5;
+                        double valor7 = doiselevadoaseis * num6;
+                        double valor8 = doiselevadoasete * num7;
+
+                        //Calcula o valor final
+                        valor_total = valor1 + valor2 + valor3 + valor4 + valor5 + valor6 + valor7 + valor8;
+
+                        //Exibe o valor final na textbox
+                        txtResultadoFinal.Text = valor_total.ToString();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Os números precisam estar entre 0 e 1.","====Erro====",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                        txtNum0.Clear();
+                        txtNum1.Clear();
+                        txtNum2.Clear();
+                        txtNum3.Clear();
+                        txtNum4.Clear();
+                        txtNum5.Clear();
+                        txtNum6.Clear();
+                        txtNum7.Clear();
+                    }
+
+                }catch(FormatException)
+                {
+                    MessageBox.Show("Entrada de dados no formato incorreto. Insira apenas números e tente novamente.", "====Erro====", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    txtNum0.Clear();
+                    txtNum1.Clear();
+                    txtNum2.Clear();
+                    txtNum3.Clear();
+                    txtNum4.Clear();
+                    txtNum5.Clear();
+                    txtNum6.Clear();
+                    txtNum7.Clear();
+                }
+            }
+        }
+
+        private void btnFechar_Click(object sender, EventArgs e)
+        {
+            //Fecha o programa
+            Close();
+        }
+
+        private void btnApagar_Click(object sender, EventArgs e)
+        {
+            txtNum0.Clear();
+            txtNum1.Clear();
+            txtNum2.Clear();
+            txtNum3.Clear();
+            txtNum4.Clear();
+            txtNum5.Clear();
+            txtNum6.Clear();
+            txtNum7.Clear();
+            txtResultadoFinal.Clear();
+
+        }
+    }
+}
